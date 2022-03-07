746c7753635e253ff5eda225eebfe12efea6cbda
Siripiri-hue <daemonprocellae@live.it>
2022-03-07 16:45:04 +0100

    Chiamata al server di una email

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 const app = new Vue ({
+    el: "#app",
+    data: {
+        emails: [],
+    },
 
-    
+    mounted () {
+        
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then ( (res) => {
+            console.log(res);
+            const item = res.data.response;
+            this.emails.push(item);
+            console.log(this.emails);
+        })
+
+    }
 })
